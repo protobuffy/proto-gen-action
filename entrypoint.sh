9ad30f98890333cb4cac75a37b2e96d8db36b64a
jchen038 <mfwinds@gmail.com>
2022-06-26 14:33:53 -0700

    display entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,11 @@
-#!/bin/sh
+#!/bin/sh -l
+
+origin=$1
+destination=$2
+
+echo "Fetching from latest origin $1"
+
+echo "Pushing to destination $1"
+# pull origin
+
+# push generated proto to dest repo
