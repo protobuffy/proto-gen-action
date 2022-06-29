002b051e655e22b88e9e833d4910bf8324bfac88
jchen038 <mfwinds@gmail.com>
2022-06-29 01:14:35 -0700

    gitignore and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-origin=$1
-destination=$2
+origin_repo=$1
+destination_repo=$2
 
-echo "Fetching from latest origin $1"
-echo "Fetching from latest origin $2"
+echo "Fetching from latest origin $origin_repo"
+echo "Fetching from latest destination $destination_repo"
 
-git clone $1 origin
-git clone $2 destination
+# clean up
+rm -rf ./origin
+rm -rf ./destination
 
+git clone $origin_repo origin
+git clone $destination_repo destination
+
+# Generate proto
+mkdir ./destination/go
+protoc --proto_path=./origin  --go_out=./destination/go/ --go_opt=paths=source_relative ./origin/*.proto ./origin/**/*.proto
+
+# Push genereted proto to destination
 echo "Pushing to destination $2"
-# pull origin
+cd origin
+commit_hash=`git rev-parse HEAD`
+cd ../destination
 
-# push generated proto to dest repo
+git add .
+git commit -m "$commit_hash"
+git push
+
+# version/tag generator
